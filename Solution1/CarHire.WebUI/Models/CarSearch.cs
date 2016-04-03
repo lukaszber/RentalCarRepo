9758d2c311362dd9e7c65ec736b1167e405aaa49
Marcin Gołda <marcon1993@gmail.com>
2016-04-03 18:52:51 +0200

    Dodałem wyszukiwanie po marce i modelu samochodu

---
+++ Solution1/CarHire.WebUI/Models/CarSearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarHire.WebUI.Models
+{
+    public class CarSearch
+    {
+        public string NameSearch { get; set; }
+        public string BrandSearch { get; set; }
+    }
+}
